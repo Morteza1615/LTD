5d3ec685d152d453bc054aebb317b8213b7211d2
Morteza1615 <mori13796@gmail.com>
2017-09-11 00:50:02 +0430

    morteza1615

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-bot_token = "183531221:AAHqvpuAkeeOEwkdtTT2g4RC87QhUUDD9es"
+bot_token = "373461992:AAHQKxI14m8pvlJbmWA-Z2qFzdadm23dGuQ"
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "1.0"
-admingp = -129471512
+admingp = -191128096
